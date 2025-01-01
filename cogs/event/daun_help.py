84535de8ce3cb6672dcab91cca185d925eb28f5b
dame <zxcdame@yandex.ru>
2025-01-01 18:05:22 +0300

    Initial commit

---
+++ cogs/event/daun_help.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+
+class Daun_help(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot == False and not message.guild:
+            await message.reply('Я не умею общаться с людьми в свободной форме. Все мои возможности описаны в /help')
+            await message.reply('Ищи меня тут: https://discord.gg/Dje3PNNJYf')
+
+async def setup(client):
+    await client.add_cog(Daun_help(client))
